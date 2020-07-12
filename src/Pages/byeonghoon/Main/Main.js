4496cd577f6cea44bd51a6bba8d33a8deb8cf9ea
bhoon-coding <bhooncoding@gmail.com>
2020-07-12 20:28:52 +0900

    [이병훈] add : mainRight 추가 및 scss nesting 완료

--- src/Pages/byeonghoon/Main/Main.js
+++ src/Pages/byeonghoon/Main/Main.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import Nav from "./Nav/Nav";
 import MainCenter from "./MainCenter/MainCenter";
+import MainRight from "./MainRight/MainRight";
+import "./Main.scss";
 
 class Main extends Component {
   render() {
     return (
       <div>
         <Nav />
-        <MainCenter />
+        <div className="byeonghoonMain">
+          <MainCenter />
+          <MainRight />
+        </div>
       </div>
     );
   }
