4b39cdc7d9e22c835d973600ac91c0d97f564342
hyeonjiya <imhyeonji0118@gmail.com>
2020-07-13 21:29:21 +0900

    Routes 주석제거

--- src/Routes.js
+++ src/Routes.js
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import hyeonjiLogin from "./Pages/hyeonji/Login/Login";
 import hyeonjiMain from "./Pages/hyeonji/Main/Main";
-// import WeunLoginBox from "./Pages/weunjeong/Login/Login";
-// import WeunMain from "./Pages/weunjeong/Main/Main";
-// import JunggyooLogin from "./Pages/junggyoo/Login/JunggyooLogin";
-// import JunggyooMain from "./Pages/junggyoo/Main/JunggyooMain";
+import WeunLoginBox from "./Pages/weunjeong/Login/Login";
+import WeunMain from "./Pages/weunjeong/Main/Main";
+import JunggyooLogin from "./Pages/junggyoo/Login/JunggyooLogin";
+import JunggyooMain from "./Pages/junggyoo/Main/JunggyooMain";
 
 class Routes extends React.Component {
   render() {
@@ -14,10 +14,10 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/login-hyeonji" component={hyeonjiLogin} />
           <Route exact path="/main-hyeonji" component={hyeonjiMain} />
-          {/* <Route exact path="/login-weunjeong" component={WeunLoginBox} />
+          <Route exact path="/login-weunjeong" component={WeunLoginBox} />
           <Route exact path="/main-weunjeong" component={WeunMain} />
           <Route exact path="/login-junggyoo" component={JunggyooLogin} />
-          <Route exact path="/main-junggyoo" component={JunggyooMain} /> */}
+          <Route exact path="/main-junggyoo" component={JunggyooMain} />
         </Switch>
       </Router>
     );
